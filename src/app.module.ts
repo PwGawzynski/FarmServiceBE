460fcd7d93e51e43863702080baee5359ec139dc
pwgawzynski <pgawzynski.backup@gmail.com>
2023-03-14 18:51:24 +0100

    added user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {AuthModule} from "./auth/auth.module";
 import {ConfigModule} from "@nestjs/config";
+import { UserModule } from './user/user.module';
 import configuration from "../config/configuration";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
       AuthModule,
+      UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],

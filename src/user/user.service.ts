72953e8d4cfcba06f9b5d14220cf00b36b3abb98
pwgawzynski <pgawzynski.backup@gmail.com>
2023-03-15 18:55:32 +0100

    created user service with methods to add new user and helper methods

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,76 @@
-import { Injectable } from '@nestjs/common';
+import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Request} from "express";
+import {User} from "./entities/user.entity";
+import {Account} from "./entities/account.entity";
+import {UserPersonalData} from "./entities/userPersonalData.entity";
+import {Address} from "../commonEntities/address.entity";
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+    _getUserIdFromReq(req: Request): string {
+        const userId = req.user['userId'];
+        if (!userId)
+            throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
+        return userId;
+    }
 
-  findAll() {
-    return `This action returns all user`;
-  }
+    async _validateUser(createUserData: CreateUserDto) {
+        const result = await User.findOne({
+            where: [
+                {
+                    email: createUserData.email,
+                },
+                {
+                    userLoginIdentificator: createUserData.userLoginIdentificator,
+                },
+            ],
+        });
+        if (result)
+            throw new HttpException(
+                `User with ${
+                    result.userLoginIdentificator === createUserData.userLoginIdentificator ? 'nickname' : 'email'
+                } already exist`,
+                HttpStatus.CONFLICT,
+            );
+    }
+    async create(createUserDto: CreateUserDto, req: Request) {
+        const userId =  this._getUserIdFromReq(req);
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+        await this._validateUser(createUserDto);
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+        const newUser = new User();
+        const newAccount = new Account();
+        const newUserPersonalData = new UserPersonalData();
+        const newAddress = new Address();
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+        newUser.id  = userId;
+        newUser.userLoginIdentificator = createUserDto.userLoginIdentificator;
+        newUser.email = createUserDto.email;
+
+        newAccount.theme = createUserDto.accountData.theme;
+
+        newUserPersonalData.name = createUserDto.userPersonalData.name;
+        newUserPersonalData.surname = createUserDto.userPersonalData.surname;
+        newUserPersonalData.phoneNumber = createUserDto.userPersonalData.phoneNumber;
+
+        newAddress.city = createUserDto.addressData.city;
+        newAddress.county = createUserDto.addressData.county;
+        newAddress.voivodeship = createUserDto.addressData.voivodeship;
+        newAddress.street = createUserDto.addressData.street;
+        newAddress.postalCode = createUserDto.addressData.postalCode;
+        newAddress.apartmentNumber = createUserDto.addressData.apartmentNumber;
+        newAddress.houseNumber = createUserDto.addressData.houseNumber;
+
+
+        newAccount.user = Promise.resolve(newUser);
+        newUserPersonalData.user = Promise.resolve(newUser);
+        newAddress.user = Promise.resolve(newUser);
+
+        await newUser.save();
+        newAddress.save();
+        newAccount.save();
+        newUserPersonalData.save();
+
+    }
 }

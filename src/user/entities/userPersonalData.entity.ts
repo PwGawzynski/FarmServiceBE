d5ce840f7477e653f5fd6e08049f2deb309d3f03
pwgawzynski <pgawzynski.backup@gmail.com>
2023-03-15 18:55:49 +0100

    created userPersonalData entity

---
+++ src/user/entities/userPersonalData.entity.ts
@@ -0,0 +1,39 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from './user.entity';
+
+/**
+ * THis column stores user personal infos like country, addresses and so on
+ */
+@Entity()
+export class UserPersonalData extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    length: 70,
+    nullable: false,
+  })
+  name: string;
+
+  @Column({
+    nullable: false,
+    length: 250,
+  })
+  surname: string;
+
+  @Column({
+    nullable: false,
+    length: 12,
+  })
+
+  phoneNumber: string;
+
+  @OneToOne(() => User, (user) => user.personalData)
+  user: Promise<User>;
+}

21a752732efe7182fe6471b2e2ce750b65e7affa
pwgawzynski <pgawzynski.backup@gmail.com>
2023-03-23 20:21:49 +0100

    added user role column and implementation of interface to class

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,13 +1,26 @@
-import {IsDefined, IsEmail, IsNotEmpty, IsNotEmptyObject, IsString, Length, ValidateNested} from "class-validator";
+import {
+    IsDefined,
+    IsEmail, IsEnum,
+    IsNotEmpty,
+    IsNotEmptyObject,
+    IsOptional,
+    IsString,
+    Length,
+    ValidateNested
+} from "class-validator";
 import {CreateUserPersonalDataDto} from "./create-userPersonalData.dto";
 import {CreateAddressDto} from "../../commonEntities/commonEntitiesDTOs/create-address.dto";
 import {CreateAccountDto} from "./create-account.dto";
 import {Type} from "class-transformer";
+import {
+    RegisterNewUserDataDtoInterfaceMobi,
+    UserRole
+} from "../../../FarmServiceTypes/User/RegisterNewUserDataDtoInterfaceMobi";
 
 /**
  * This DTO contains information about User entity properties types
  */
-export class CreateUserDto {
+export class CreateUserDto implements RegisterNewUserDataDtoInterfaceMobi{
     @IsString({
         message: "User login/email/nip must be string type"
     })
@@ -15,7 +28,8 @@ export class CreateUserDto {
         message: 'User login/email/nip cannot be empty string'
     })
     @Length(0,40)
-    userLoginIdentificator:string;
+    @IsOptional()
+    userLoginIdentificator?:string;
 
 
     @IsString({
@@ -27,6 +41,11 @@ export class CreateUserDto {
     @Length(0,350)
     @IsEmail()
     email: string;
+
+
+    @IsEnum(UserRole)
+    userRole: UserRole;
+
 
     @IsNotEmptyObject()
     @IsDefined()

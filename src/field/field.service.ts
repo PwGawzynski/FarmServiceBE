d7d8cbfbe369098569f136412d49642428582241
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-07 21:58:35 +0100

    added field service

---
+++ src/field/field.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+import { CreateFieldDto } from './dto/create-field.dto';
+import { UpdateFieldDto } from './dto/update-field.dto';
+
+@Injectable()
+export class FieldService {}

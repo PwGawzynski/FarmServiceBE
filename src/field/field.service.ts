647bcbb249ea6af13197c4a6324e8336d658a137
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-07 21:58:55 +0100

    rem unused

--- src/field/field.service.ts
+++ src/field/field.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateFieldDto } from './dto/create-field.dto';
-import { UpdateFieldDto } from './dto/update-field.dto';
 
 @Injectable()
 export class FieldService {}

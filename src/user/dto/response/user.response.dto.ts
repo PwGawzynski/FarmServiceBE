12c237dc626750b3013a309fca78fb377bf38d28
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-11 19:45:18 +0100

    changed to whiteList

--- src/user/dto/response/user.response.dto.ts
+++ src/user/dto/response/user.response.dto.ts
@@ -1,16 +1,28 @@
-import { UserRole } from '../../../../FarmServiceTypes/User/RegisterNewUserDataDtoInterfaceMobi';
 import { UserPersonalDataResponseDto } from './userPersonalData.response.dto';
 import { AddressResponseDto } from '../../../commonEntities/dto/response/address.response.dto';
 import { AccountResponseDto } from './account.response';
-import { GetUserDataResponse } from '../../../../FarmServiceTypes/respnse/UserService/GetUserDataResponse';
+import { UserRole } from '../../../../FarmServiceTypes/User/Enums';
+import { Expose } from 'class-transformer';
 
-export class UserResponseDto implements GetUserDataResponse {
+export class UserResponseWhiteListDto {
+  constructor(partial: Partial<UserResponseWhiteListDto>) {
+    Object.assign(this, partial);
+  }
+  @Expose()
   email: string;
+  @Expose()
   role: UserRole;
+  @Expose()
   personalData: UserPersonalDataResponseDto;
+  @Expose()
   address: AddressResponseDto;
+  @Expose()
   account: AccountResponseDto;
+}
+
+export class UserResponseDto extends UserResponseWhiteListDto {
   constructor(partial: Partial<UserResponseDto>) {
+    super(partial);
     Object.assign(this, partial);
   }
 }

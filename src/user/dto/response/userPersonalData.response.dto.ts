fa3c1e18a81a164df0c5078dd593b66d47810449
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-11 19:45:20 +0100

    changed to whiteList

--- src/user/dto/response/userPersonalData.response.dto.ts
+++ src/user/dto/response/userPersonalData.response.dto.ts
@@ -1,18 +1,28 @@
-import { Exclude } from 'class-transformer';
+import { Exclude, Expose } from 'class-transformer';
 import { GetUserPersonalDataResponse } from '../../../../FarmServiceTypes/respnse/UserService/GetUserDataResponse';
 
+export class UserPersonalDataWhiteList {
+  constructor(partial: Partial<UserPersonalDataWhiteList>) {
+    Object.assign(this, partial);
+  }
+
+  @Expose()
+  name: string;
+  @Expose()
+  surname: string;
+  @Expose()
+  phoneNumber: string;
+}
+
 export class UserPersonalDataResponseDto
+  extends UserPersonalDataWhiteList
   implements GetUserPersonalDataResponse
 {
   @Exclude()
   id: string;
-  name: string;
-
-  surname: string;
-
-  phoneNumber: string;
 
   constructor(partial: Partial<UserPersonalDataResponseDto>) {
+    super(partial);
     Object.assign(this, partial);
   }
 }

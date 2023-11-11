83206f43ebb22366b056ef3f0490f4e553b87b74
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-11 13:03:47 +0100

    due to this generic has builtin converter class to interface changed to class

--- src/user/dto/response/account.response.spec.ts
+++ src/user/dto/response/account.response.spec.ts
@@ -1,13 +1,13 @@
 import { Equal } from '../../../../TypeScriptHelpers/dtoToInterface';
-import { AccountResponseWhiteList } from './account.response';
+import { AccountWhiteList } from './account.response';
 import { AccountResponseBase } from '../../../../FarmServiceTypes/Account/Ressponses';
 
 /**
  * This test make sens because type which can be assigned to test var,
  * depends on Equal operator which checks if interfaces are equal
  */
-const test: Equal<AccountResponseWhiteList, AccountResponseBase> = true;
+const test: Equal<AccountWhiteList, AccountResponseBase> = true;
 
-describe('IsAccountAndDtoEqual', () => {
+describe('Are AccountResponseBase whitelist match DTO', () => {
   it('should be true', () => expect(test).toBe(true));
 });

5140b7dad8c8f64ce25d2f8360419b549b7b75fc
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-13 16:11:45 +0100

    created post path to test owner guard

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { CompanyService } from './company.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guards';
+import { Owner } from '../../decorators/roles.decorator';
+import { RolesGuard } from '../../Guards/RoleGuard';
 
 @Controller('company')
 export class CompanyController {
   constructor(private readonly companyService: CompanyService) {}
+
+  @Get()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Owner()
+  async isOwner() {
+    console.log('dziala');
+  }
 }

96f91021b1a6564bebb98bd2be0b85b2669a2a09
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-14 22:47:04 +0100

    done saving in db

--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,10 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCompanyDto } from './dto/create-company.dto';
+import { User } from '../user/entities/user.entity';
+import { Company } from './entities/company.entity';
+import * as console from 'console';
+import { CompanyAddress } from '../commonEntities/company-address.entity';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class CompanyService {
-  async create(createCompanyDto: CreateCompanyDto) {
-    console.log(createCompanyDto);
-    return 'oks';
+  async create(createCompanyDto: CreateCompanyDto, user: User) {
+    const newCompany = new Company();
+    const newAddress = new CompanyAddress();
+
+    newCompany.name = createCompanyDto.name;
+    newCompany.owner = Promise.resolve(user);
+    await newCompany.save();
+
+    newAddress.city = createCompanyDto.address.city;
+    newAddress.county = createCompanyDto.address.county;
+    newAddress.street = createCompanyDto.address.street;
+    newAddress.voivodeship = createCompanyDto.address.voivodeship;
+    newAddress.houseNumber = createCompanyDto.address.houseNumber;
+    newAddress.apartmentNumber = createCompanyDto.address.apartmentNumber;
+    newAddress.postalCode = createCompanyDto.address.postalCode;
+    newAddress.company = Promise.resolve(newCompany);
+
+    await newAddress.save();
+
+    newCompany.address = Promise.resolve(newAddress);
+    newCompany.save();
+
+    console.log(newCompany);
+    // TODO return DTO
+    return 'okks';
   }
 }

1cf89d2c9692b22e97468b62dd1313e12bc80386
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-14 15:27:06 +0100

    created @Public, @AllowOnlyByToken decorators

--- decorators/auth.decorators.ts
+++ decorators/auth.decorators.ts
@@ -1,5 +1,10 @@
 import { SetMetadata } from '@nestjs/common';
 import { UserRole } from '../FarmServiceTypes/User/Enums';
+import { JwtAuthGuard } from '../src/auth/jwt-auth.guards';
 
 export const Owner = () => SetMetadata('roles', [UserRole.owner]);
 export const Worker = () => SetMetadata('roles', [UserRole.worker]);
+
+export const Public = () => SetMetadata(JwtAuthGuard.IS_PUBLIC_PATH, true);
+export const AllowOnlyByToken = () =>
+  SetMetadata(JwtAuthGuard.IS_CREATE_USER_ACTION, true);

c2981ddc3f6d415ccf39b16079e3f5c561329712
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-14 15:27:50 +0100

    Post in progress

--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { CreateCompanyDto } from './dto/create-company.dto';
 
 @Injectable()
-export class CompanyService {}
+export class CompanyService {
+  async create(createCompanyDto: CreateCompanyDto) {
+    console.log(createCompanyDto);
+    return 'oks';
+  }
+}

1c369287250fbeafb084857683986665c989633f
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-19 17:25:55 +0100

    created Path to get all fields

--- src/field/field.controller.ts
+++ src/field/field.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { FieldService } from './field.service';
 import { Client } from '../../decorators/auth.decorators';
 import { GetUser } from '../../decorators/user.decorators';
 import { User } from '../user/entities/user.entity';
 import { CreateFieldDto } from './dto/create-field.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { use } from 'passport';
 
 @ApiTags('Field')
 @Controller('field')
@@ -18,5 +19,11 @@ export class FieldController {
     @GetUser() user: User,
   ) {
     return this.fieldService.createByOwner(createFieldDto, user);
+  }
+
+  @Get('all')
+  @Client()
+  async getAllFields(@GetUser() user: User) {
+    return this.fieldService.getAllFields(user);
   }
 }

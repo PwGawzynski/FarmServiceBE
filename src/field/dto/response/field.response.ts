da22d6a10528a8d1c9e09facd3f61ba41b68b499
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-19 17:00:17 +0100

    created field response dto

---
+++ src/field/dto/response/field.response.ts
@@ -0,0 +1,30 @@
+import { Exclude, Expose } from 'class-transformer';
+
+export class FieldResponseWhiteList {
+  constructor(partial: Partial<FieldResponseDto>) {
+    Object.assign(this, partial);
+  }
+
+  @Expose()
+  polishSystemId: string;
+
+  @Expose()
+  area: number;
+
+  @Expose()
+  dateOfCollectionData: Date;
+
+  @Expose()
+  addressId: string;
+
+  @Expose()
+  owner_id: string;
+}
+
+@Exclude()
+export class FieldResponseDto extends FieldResponseWhiteList {
+  constructor(partial: Partial<FieldResponseDto>) {
+    super(partial);
+    Object.assign(this, partial);
+  }
+}

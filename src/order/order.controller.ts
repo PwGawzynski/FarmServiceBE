3a407804eaa5857fdea7075cd3911d6de9d1b49b
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-20 10:18:39 +0100

    created POST method for order creation

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,9 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { ApiTags } from '@nestjs/swagger';
+import { Owner } from '../../decorators/auth.decorators';
+import { GetOwnedCompany } from '../../decorators/user.decorators';
+import { Company } from '../company/entities/company.entity';
+import { CreateOrderDto } from './dto/create-order.dto';
 
 @ApiTags('Order')
 @Controller('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
+
+  @Post()
+  @Owner()
+  async create(
+    @Body() createOrderDto: CreateOrderDto,
+    @GetOwnedCompany() company: Company,
+  ) {
+    return this.orderService.create(createOrderDto, company);
+  }
 }

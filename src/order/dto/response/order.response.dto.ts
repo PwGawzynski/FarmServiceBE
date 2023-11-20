5e9bd1ce28c6558c6ba3269c10fd389d4c43183f
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-20 10:20:31 +0100

    created order response

---
+++ src/order/dto/response/order.response.dto.ts
@@ -0,0 +1,39 @@
+import { Exclude, Expose } from 'class-transformer';
+import {
+  OrderStatus,
+  ServiceType,
+} from '../../../../FarmServiceTypes/Order/Enums';
+
+export class OrderResponseWhiteList {
+  constructor(partial: Partial<OrderResponseDto>) {
+    Object.assign(this, partial);
+  }
+  @Expose()
+  name: string;
+  @Expose()
+  status: OrderStatus;
+  @Expose()
+  serviceType: ServiceType;
+  @Expose()
+  performanceDate: Date;
+  @Expose()
+  totalArea?: string;
+  @Expose()
+  createdAt?: Date;
+  @Expose()
+  openedAt?: Date;
+  @Expose()
+  additionalInfo: string;
+  @Expose()
+  pricePerUnit?: number;
+  @Expose()
+  clientId: string;
+}
+
+@Exclude()
+export class OrderResponseDto extends OrderResponseWhiteList {
+  constructor(partial: Partial<OrderResponseDto>) {
+    super(partial);
+    Object.assign(this, partial);
+  }
+}

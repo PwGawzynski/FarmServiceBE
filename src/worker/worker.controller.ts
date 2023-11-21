826c5b780a5b05d9d7d3c724d98fca32a515d1b9
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-21 13:15:27 +0100

    created POST/create for create worker action

--- src/worker/worker.controller.ts
+++ src/worker/worker.controller.ts
@@ -1,9 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { WorkerService } from './worker.service';
 import { ApiTags } from '@nestjs/swagger';
+import { Owner } from '../../decorators/auth.decorators';
+import { CreateWorkerDto } from './dto/create-worker.dto';
+import { GetOwnedCompany } from '../../decorators/user.decorators';
+import { Company } from '../company/entities/company.entity';
 
 @ApiTags('Worker')
 @Controller('worker')
 export class WorkerController {
   constructor(private readonly workerService: WorkerService) {}
+
+  @Post()
+  @Owner()
+  async create(
+    @Body() createWorkerDto: CreateWorkerDto,
+    @GetOwnedCompany() company: Company,
+  ) {
+    return this.workerService.create(createWorkerDto, company);
+  }
 }

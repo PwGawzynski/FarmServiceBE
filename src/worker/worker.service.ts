777a02f76d9cee0173c7bcd22f253ba8d0658e05
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-21 13:15:56 +0100

    added logic for worker add

--- src/worker/worker.service.ts
+++ src/worker/worker.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { CreateWorkerDto } from './dto/create-worker.dto';
+import { Company } from '../company/entities/company.entity';
+import { Worker } from './entities/worker.entity';
+import {
+  ResponseCode,
+  ResponseObject,
+} from '../../FarmServiceTypes/respnse/responseGeneric';
 
 @Injectable()
-export class WorkerService {}
+export class WorkerService {
+  async create(createWorkerDto: CreateWorkerDto, company: Company) {
+    const newWorker = new Worker({
+      company: Promise.resolve(company),
+      user: Promise.resolve(createWorkerDto.user),
+    });
+    await newWorker._shouldNotExist();
+    newWorker.save();
+    return { code: ResponseCode.ProcessedCorrect } as ResponseObject;
+  }
+}

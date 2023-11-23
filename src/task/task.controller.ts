7408f74180f774f714ee5d18fa8fcc95d6398133
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-23 16:02:53 +0100

    added path to add task

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,9 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { ApiTags } from '@nestjs/swagger';
+import { Owner } from '../../decorators/auth.decorators';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetOwnedCompany } from '../../decorators/user.decorators';
+import { Company } from '../company/entities/company.entity';
 
 @ApiTags('Taskcontroller ')
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
+
+  @Post()
+  @Owner()
+  async create(
+    @Body() createTaskDto: CreateTaskDto,
+    @GetOwnedCompany() company: Company,
+  ) {
+    return this.taskService.create(createTaskDto, company);
+  }
 }

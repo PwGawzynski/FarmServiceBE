894d9d89ed8488da3db4527266db51be2829e480
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-23 21:17:23 +0100

    created path to get all tasks for company and all tasks for order

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,12 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { ApiTags } from '@nestjs/swagger';
 import { Owner } from '../../decorators/auth.decorators';
-import { CreateTaskDto } from './dto/create-task.dto';
+import { CrateTaskCollection } from './dto/create-task.dto';
 import { GetOwnedCompany } from '../../decorators/user.decorators';
 import { Company } from '../company/entities/company.entity';
 
-@ApiTags('Taskcontroller ')
+@ApiTags('TaskController ')
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
@@ -14,9 +14,24 @@ export class TaskController {
   @Post()
   @Owner()
   async create(
-    @Body() createTaskDto: CreateTaskDto,
+    @Body() createTaskCollection: CrateTaskCollection,
     @GetOwnedCompany() company: Company,
   ) {
-    return this.taskService.create(createTaskDto, company);
+    return this.taskService.create(createTaskCollection, company);
+  }
+
+  @Get('all')
+  @Owner()
+  async all(@GetOwnedCompany() company: Company) {
+    return this.taskService.all(company);
+  }
+
+  @Get('by-order/')
+  @Owner()
+  async allByOrder(
+    @GetOwnedCompany() company: Company,
+    @Param('id') id: string,
+  ) {
+    return this.taskService.allByOrder(company, id);
   }
 }

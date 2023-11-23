dada5461061b05e6c5bf7163767f1fc8ddf2a58e
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-23 16:02:56 +0100

    added path to add task

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Task } from './entities/task.entity';
+import { Company } from '../company/entities/company.entity';
+import {
+  ResponseCode,
+  ResponseObject,
+} from '../../FarmServiceTypes/respnse/responseGeneric';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  async create(createTaskDto: CreateTaskDto, company: Company) {
+    const newTask = new Task({
+      ...createTaskDto,
+      field: Promise.resolve(createTaskDto.field),
+      worker: Promise.resolve(createTaskDto.worker),
+      order: Promise.resolve(createTaskDto.order),
+    });
+    await newTask._shouldBeValid(company);
+    newTask.save();
+    return {
+      code: ResponseCode.ProcessedCorrect,
+    } as ResponseObject;
+  }
+}

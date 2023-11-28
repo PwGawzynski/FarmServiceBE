84f1b7df00b1b356eb4145d4176b06861ec72da3
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-28 09:09:56 +0100

    rem implement

--- src/worker/dto/create-worker.dto.ts
+++ src/worker/dto/create-worker.dto.ts
@@ -1,16 +1,8 @@
-import { OmitBaseEntityAndId } from '../../../SelfTypes/OmitBaseEntity';
-import { Worker } from '../entities/worker.entity';
 import { IsUUID } from 'class-validator';
 import { FindOrReject } from '../../../ClassValidatorCustomDecorators/FindOrReject.decorator';
 import { User } from '../../user/entities/user.entity';
 
-export class CreateWorkerDto
-  implements
-    OmitBaseEntityAndId<
-      Worker,
-      'company' | 'user' | 'tasks' | '_shouldNotExist'
-    >
-{
+export class CreateWorkerDto {
   @IsUUID()
   @FindOrReject(User, { message: 'Cannot find given user' })
   user: User;

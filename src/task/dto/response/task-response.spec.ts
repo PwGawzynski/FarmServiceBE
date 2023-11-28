25f18320d88c8cdda91f2490bf1a13ac20ebc583
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-28 21:43:12 +0100

    added omit

--- src/task/dto/response/task-response.spec.ts
+++ src/task/dto/response/task-response.spec.ts
@@ -1,9 +1,9 @@
 import { Equal } from '../../../../TypeScriptHelpers/dtoToInterface';
-import { CreateTaskResponseDto } from './create-task.response.dto';
+import { TaskResponseDto } from './task-response.dto';
 import { TaskResponseBase } from '../../../../FarmServiceTypes/Task/Restonses';
 
 const test: Equal<
-  Omit<CreateTaskResponseDto, 'field' | 'worker'>,
+  Omit<TaskResponseDto, 'field' | 'worker'>,
   Omit<TaskResponseBase, 'field' | 'worker'>
 > = true;
 

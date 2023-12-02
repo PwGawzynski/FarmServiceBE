a86683012c5e211550f3ca277119b2034dd8a6d8
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-02 20:54:13 +0100

    created update dto

--- src/order/dto/update-order.dto.ts
+++ src/order/dto/update-order.dto.ts
@@ -1,12 +1,18 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateOrderDto } from './create-order.dto';
-import { IsEnum, IsNumber, Max, Min } from 'class-validator';
+import { IsEnum, IsNumber, IsOptional, Max, Min } from 'class-validator';
 import OrderConstants from '../../../FarmServiceTypes/Order/Constants';
 import { OrderStatus } from '../../../FarmServiceTypes/Order/Enums';
+import { Order } from '../entities/order.entity';
+import { FindOrReject } from '../../../ClassValidatorCustomDecorators/FindOrReject.decorator';
 
 export class UpdateOrderDto extends PartialType(CreateOrderDto) {
+  @FindOrReject(Order, { message: 'Cannot find order with given status' })
+  order: Order;
+
   @IsEnum(OrderStatus)
-  status: OrderStatus;
+  @IsOptional()
+  status?: OrderStatus;
 
   @IsNumber({ maxDecimalPlaces: 2 })
   @Min(OrderConstants.MIN_PRICE_PER_UNIT)

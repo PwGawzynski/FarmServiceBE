3ce88f4cf2db0c91d2b346ab9a302aa1a7763e92
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-06 10:24:59 +0100

    changed notifications columns

--- src/notifications/dto/create-notification.dto.ts
+++ src/notifications/dto/create-notification.dto.ts
@@ -1,20 +1,13 @@
 import { OmitBaseEntityAndId } from '../../../SelfTypes/OmitBaseEntity';
 import { Notification } from '../entities/notification.entity';
-import {
-  IsArray,
-  IsEnum,
-  IsNotEmpty,
-  IsString,
-  IsUUID,
-  Length,
-} from 'class-validator';
+import { IsEnum, IsNotEmpty, IsString, IsUUID, Length } from 'class-validator';
 import { FindOrReject } from '../../../ClassValidatorCustomDecorators/FindOrReject.decorator';
 import { User } from '../../user/entities/user.entity';
 import { EventType } from '../../../FarmServiceTypes/Notification/Enums';
 import NotificationConstants from '../../../FarmServiceTypes/Notification/Constants';
 
 export class CreateNotificationDto
-  implements OmitBaseEntityAndId<Notification, 'causer' | 'recipients'>
+  implements OmitBaseEntityAndId<Notification, 'causer'>
 {
   @IsString({ message: 'schort info must be a string type' })
   // TODO Check if without isNotEmpty validation will be still the same because length should provide check if is not empty <1,100> etc
@@ -45,14 +38,14 @@ export class CreateNotificationDto
 
   @IsUUID()
   @FindOrReject(User, { message: 'Cannot find user with given id' })
-  causer: string;
+  causer: User;
 
-  @IsArray({ message: 'Recipients must be an array of UserID type' })
+  /*@IsArray({ message: 'Recipients must be an array of UserID type' })
   @IsUUID(undefined, { each: true })
   @FindOrReject(User, {
     each: true,
     message: (validationArguments) =>
       `Cannot find user with ${validationArguments.value} id`,
   })
-  recipients: Array<string>;
+  recipients: Array<string>;*/
 }

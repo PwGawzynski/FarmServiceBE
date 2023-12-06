15e26fb8e267152bc6cf4002dfdfe55927f39216
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-06 10:25:52 +0100

    created sse endpoint

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,8 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Sse } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { ApiTags } from '@nestjs/swagger';
+import { Owner } from '../../decorators/auth.decorators';
+import { GetOwnedCompany } from '../../decorators/user.decorators';
+import { Company } from '../company/entities/company.entity';
 @ApiTags('Notification')
 @Controller('notifications')
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
+
+  @Get('sse')
+  @Sse()
+  @Owner()
+  async getTasksSse(@GetOwnedCompany() company: Company) {
+    return this.notificationsService.getTasksSse(company);
+  }
 }

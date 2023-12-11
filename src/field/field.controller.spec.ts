a7de47f6f33d1575c629427b74184baef8e00003
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-11 11:44:55 +0100

    added tests

--- src/field/field.controller.spec.ts
+++ src/field/field.controller.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FieldController } from './field.controller';
 import { FieldService } from './field.service';
+import { HttpModule } from '@nestjs/axios';
 
 describe('FieldController', () => {
   let controller: FieldController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        HttpModule.registerAsync({
+          useFactory: () => ({
+            timeout: 5000,
+            maxRedirects: 5,
+          }),
+        }),
+      ],
       controllers: [FieldController],
       providers: [FieldService],
     }).compile();
